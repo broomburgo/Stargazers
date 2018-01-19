4dc6f4ace278c747f93c896463222836d3e2c019
Elviro Rocca <broomburgo@gmail.com>
2018-01-19 13:42:09 +0100

    Initial Commit

---
+++ Stargazers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stargazers
+//
+//  Created by Elviro Rocca on 19/01/18.
+//  Copyright © 2018 Elviro Rocca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
